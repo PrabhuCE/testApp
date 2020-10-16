4fbd8f0822f4f22655adcc75df5bfa845140e2aa
dhanaprabhu <dhana.prabhu@continualengine.com>
2020-10-16 12:11:51 +0530

    test first deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Test app new</div>
+        <div>Test app attempt 2</div>
       </header>
     </div>
   );
