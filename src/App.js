55a05fe1ffffc2168bc9f9a50c7b0d8479a9a7d5
dhanaprabhu <dhana.prabhu@continualengine.com>
2020-10-16 11:14:20 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Test app New</div>
+        <div>Test app</div>
       </header>
     </div>
   );
